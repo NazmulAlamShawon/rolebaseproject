3d84e4275ed40575b5e114440a15d06f7d2a6e35
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-10 10:47:00 +0600

    add new test

--- app/CreateUser/page.jsx
+++ app/CreateUser/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const CreateUser = () => {
   return (
-    <div><h1> Create User</h1></div>
+    <div>
+      <h1> Create User</h1>
+      <div> </div>
+      <div> </div>
+    
+    </div>
   )
 }
 
